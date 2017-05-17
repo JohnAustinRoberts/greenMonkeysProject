3369ec423dacf410443d5866f8f3e2d5cacb07d5
Mark A Lang <mlang12@Marks-MacBook-Pro.local>
2017-05-16 21:10:09 -0400

    CORS fix

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -156,7 +156,7 @@ $(document).ready(function(){
   //terms are expected to be passed in space delimited format
   function makeSearchIntoFoodURL(userSearch){
     var srch = userSearch.replace(/\s/g, "%20");
-    return "http://food2fork.com/api/search?key=" + apiKeys.food + 
+    return "https://crossorigin.me/http://food2fork.com/api/search?key=" + apiKeys.food + 
       "&q=" + srch;
   }
 
